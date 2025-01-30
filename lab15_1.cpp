80c514e3b56c9f041b53de9218f9ec41852e1e1f
Tanida <printer.rung@gmail.com>
2025-01-30 13:46:07 +0700

    lab15_1 completed

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,34 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+void show(int a,char b,char &c,int *x,char *y,int *z){
+    cout << a << " " << b << " " << c << " "<< x << " "<< (int *)y << " " << z << "\n";
+}
 
-	
-	
-	return 0;
+
+int main(){
+    int a=5;
+    char b='A';
+    char &c=b;
+    int *x=&a;
+    char *y=&b;
+    int *z=x;
+
+    cout << a << " " << b << " "<< c << " " << x << " " <<  &y << " " << z << "\n";
+    cout << &a << " " << (int *)&b << " " << (int *)&c << " " << &x << " " << &y << " " << &z << "\n";
+
+    c='F';
+    show(a,b,c,x,y,z);
+
+    *y='W';
+    show(a,b,c,x,y,z);
+    
+    *x=6;
+    show(a,b,c,x,y,z);
+
+    *z=7;
+    show(a,b,c,x,y,z);
+
+
+    return 0;
 }
